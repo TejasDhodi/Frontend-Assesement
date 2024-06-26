30ac63cfab97480437f563047f0ecf746e3e5077
Tejas Dhodi <tejasdhodi77@gmail.com>
2024-06-26 19:37:53 +0530

    Improve Responsiveness

--- app.js
+++ app.js
@@ -8,43 +8,69 @@ const answers = document.querySelector('.answer');
 console.log(accordian);
 
 accordian.forEach(currAccordian => {
-    currAccordian.addEventListener('click', () => {
-        accordian.forEach(otherAccordian => {
-            otherAccordian.classList.remove('active');
-        });
-        currAccordian.classList.add('active');
+  currAccordian.addEventListener('click', () => {
+    accordian.forEach(otherAccordian => {
+      otherAccordian.classList.remove('active');
     });
+    currAccordian.classList.add('active');
+  });
 });
 
 
 // --------------- For Carousel ------------------
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  breakpoints: { 
+    200: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-  });
+    700: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+    },
+    1025: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+    },
+},
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 var swiper = new Swiper(".mySwiper1", {
-    slidesPerView: 3,
-    spaceBetween: 70,
-    pagination: {
-      el: ".swiper-pagination1",
-      clickable: true,
+  slidesPerView: 3,
+  spaceBetween: 70,
+  breakpoints: { 
+    641: {
+        slidesPerView: 2,
+        spaceBetween: 20,
     },
-  });
+},
+  pagination: {
+    el: ".swiper-pagination1",
+    clickable: true,
+  },
+});
 
 var swiper = new Swiper(".mySwiper2", {
-    slidesPerView: 3,
-    spaceBetween: 70,
-    pagination: {
-      el: ".swiper-pagination2",
-      clickable: true,
+  slidesPerView: 3,
+  spaceBetween: 70,
+  breakpoints: { 
+    641: {
+        slidesPerView: 2,
+        spaceBetween: 20,
     },
-  });
+},
+  pagination: {
+    el: ".swiper-pagination2",
+    clickable: true,
+  },
+});
 
 
 
@@ -52,6 +78,24 @@ var swiper = new Swiper(".mySwiper2", {
 const na1Ham = document.querySelector('.nav1Ham');
 const nav1menu = document.querySelector('#navInfoContainer');
 
-na1Ham.addEventListener('click', () => {
-    nav1menu.classList.toggle('downNav1')
-})
+// For  Nav 1 Hamburger
+const nav2Ham = document.querySelector('.nav2Hamburger');
+const nav2menu = document.querySelector('.navItems');
+const nav2 = document.querySelector('#nav2')
+
+
+const toggleNavs = (elem1, elem2, className) => {
+  elem1.addEventListener('click', () => {
+    elem2.classList.toggle(className)
+
+    console.log(elem1, elem2, className);
+  })
+}
+
+toggleNavs(na1Ham, nav1menu, 'downNav1');
+toggleNavs(nav2Ham, nav2menu, 'showNav2');
+toggleNavs(nav2Ham, nav2, 'showNav2');
+
+// na1Ham.addEventListener('click', () => {
+//   nav1menu.classList.toggle('downNav1')
+// })
